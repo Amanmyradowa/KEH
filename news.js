ea76fd0c8fa195b643a4c45b7ee361572a590306
Imdat Developers <imdatdevelopers@Imdats-iMac.local>
2023-09-19 17:17:13 +0500

    responsive beginning

--- news.js
+++ news.js
@@ -25,41 +25,6 @@ const newsItems = [
   },
 ]
 
-let news_items = document.querySelector(".news__items");
-
-function setNews() {
-  
-  for(let i=0; i<newsItems.length; i++) {
-    const div = document.createElement('div');
-    div.setAttribute('class', 'item text-color-dark-gray font-size-20 font-weight-700 padding-20-12-20-10 border-bottom-grey');
-    div.textContent = newsItems[i].text;
-    news_items.appendChild(div);
-  }
-}
-
-setNews();
-
-function changeNews() {
-  let news = document.querySelector(".news");
-  
-  let newsImage = document.querySelector(".news__image");
-  
-  let img = document.createElement('img');
-  img.setAttribute('src', `./assets/images/${newsDatas[0].img}`);
-  img.setAttribute('class', 'image__element');
-  newsImage.appendChild(img);
-  
-  let title = document.createElement('h3');
-  title.setAttribute('class', 'news__heading text-color-dark-gray font-size-20 font-weight-700 margin-bottom-18');
-  title.textContent = newsDatas[0].title;
-  news.appendChild(title);
-  
-  let date = document.createElement('div');
-  date.setAttribute('class', 'date font-size-12 text-color-dark-gray margin-bottom-10');
-  date.textContent = newsDatas[0].date;
-  news.appendChild(date);
-}
-
 
 let newsDatas = [
   {
@@ -67,7 +32,84 @@ let newsDatas = [
     title: 'В Ашхабаде планируется построить автомобильный мост на пересечении улицы Г.Кулиева и ЖК «Тазе Заман»',
     date: '11.04.2023',
     text: 'Министерство строительства и архитектуры Туркменистана объявило международный конкурс на подготовку и строительство проекта нового автомобильного моста на пересечении улицы Г.Кулиева и жилого комплекса «Тазе Заман» в г. Ашхабаде. Конкурс проводится по поручению Государственного агентства по управлению строительством автомобильных дорог. Заявки и предложения заинтересованных лиц принимаются и оформляются в отделе маркетинговых исследований и внешнеэкономических связей Министерства строительства и архитектуры. Заявки принимаются до 10 октября 2023 года по адресу: г. Ашхабад, проспект Арчабиль, 84. Для связи можно обращаться по телефонам: 44-46-64, 44-46-77.'
+  },
+  {
+    img: 'Rectangle 253.png',
+    title: 'В Ашхабаде планируется построить автомобильный мост на пересечении улицы Г.Кулиева и ЖК «Тазе Заман»',
+    date: '11.04.2023',
+    text: 'Министерство строительства и архитектуры Туркменистана объявило международный конкурс на подготовку и строительство проекта нового автомобильного моста на пересечении улицы Г.Кулиева и жилого комплекса «Тазе Заман» в г. Ашхабаде. Конкурс проводится по поручению Государственного агентства по управлению строительством автомобильных дорог. Заявки и предложения заинтересованных лиц принимаются и оформляются в отделе маркетинговых исследований и внешнеэкономических связей Министерства строительства и архитектуры. Заявки принимаются до 10 октября 2023 года по адресу: г. Ашхабад, проспект Арчабиль, 84. Для связи можно обращаться по телефонам: 44-46-64, 44-46-77.'
+  },
+  {
+    img: 'Rectangle 253.png',
+    title: 'В Ашхабаде планируется построить автомобильный мост на пересечении улицы Г.Кулиева и ЖК «Тазе Заман»',
+    date: '11.04.2023',
+    text: 'Министерство строительства и архитектуры Туркменистана объявило международный конкурс на подготовку и строительство проекта нового автомобильного моста на пересечении улицы Г.Кулиева и жилого комплекса «Тазе Заман» в г. Ашхабаде. Конкурс проводится по поручению Государственного агентства по управлению строительством автомобильных дорог. Заявки и предложения заинтересованных лиц принимаются и оформляются в отделе маркетинговых исследований и внешнеэкономических связей Министерства строительства и архитектуры. Заявки принимаются до 10 октября 2023 года по адресу: г. Ашхабад, проспект Арчабиль, 84. Для связи можно обращаться по телефонам: 44-46-64, 44-46-77.'
+  },
+  {
+    img: 'KEH-270.png',
+    title: 'В Ашхабаде планируется построить автомобильный мост на пересечении улицы Г.Кулиева и ЖК «Тазе Заман»',
+    date: '11.04.2023',
+    text: 'Министерство строительства и архитектуры Туркменистана объявило международный конкурс на подготовку и строительство проекта нового автомобильного моста на пересечении улицы Г.Кулиева и жилого комплекса «Тазе Заман» в г. Ашхабаде. Конкурс проводится по поручению Государственного агентства по управлению строительством автомобильных дорог. Заявки и предложения заинтересованных лиц принимаются и оформляются в отделе маркетинговых исследований и внешнеэкономических связей Министерства строительства и архитектуры. Заявки принимаются до 10 октября 2023 года по адресу: г. Ашхабад, проспект Арчабиль, 84. Для связи можно обращаться по телефонам: 44-46-64, 44-46-77.'
   }
-]
+];
 
-changeNews();
+
+let news_items = document.querySelector(".news__items");
+
+function setData(id) {
+  
+  for(let i=0; i<newsItems.length; i++) {
+    const div = document.createElement('div');
+    div.setAttribute('class', 'item text-color-dark-gray font-size-20 font-weight-700 padding-20-12-20-10 border-bottom-grey');
+    div.setAttribute('id', `${i}`);
+    div.textContent = newsItems[i].text;
+    news_items.appendChild(div);
+  }
+  let items = document.querySelectorAll(".item");
+
+
+  for(let i=0; i<items.length; i++) {
+    items[i].addEventListener("click", ()=>changeNews(i));
+  }
+
+  setLeftData(0)
+
+}
+
+function setLeftData(id) {
+
+  let news = document.querySelector(".news");
+  news.innerHTML = "";
+  
+  let newsImage = document.createElement("div");
+  newsImage.setAttribute('class', 'news__image');
+  
+  let img = document.createElement('img');
+  img.setAttribute('src', `./assets/images/${newsDatas[id].img}`);
+  img.setAttribute('class', 'image__element');
+  newsImage.appendChild(img);
+  news.appendChild(newsImage);
+
+  
+  let title = document.createElement('h3');
+  title.setAttribute('class', 'news__heading text-color-dark-gray font-size-20 font-weight-700 margin-bottom-18');
+  title.textContent = newsDatas[id].title;
+  news.appendChild(title);
+  
+  let date = document.createElement('div');
+  date.setAttribute('class', 'date font-size-12 text-color-dark-gray margin-bottom-10');
+  date.textContent = newsDatas[id].date;
+  news.appendChild(date);
+
+  let text = document.createElement('p');
+  text.setAttribute('class', 'text-color-dark-gray font-size-14');
+  text.textContent = newsDatas[id].text;
+  news.appendChild(text);
+
+}
+
+setData(0);
+
+function changeNews(id) {
+  setLeftData(id);
+}
--- script.js
+++ script.js
@@ -7,8 +7,31 @@ for(let i=0; i<accordionButtonContainer.length; i++){
 
     if(accordion[i].style.height === "90px") {
       accordion[i].style.height = "0"
-    } else{
+    }else{
       accordion[i].style.height = "90px"
     }
+
   })
 };
+
+document.addEventListener("click", function(e) {
+  let accordionButtonContainer = document.querySelectorAll(".accordion-button-container");
+  let accordion = document.querySelectorAll(".accordion");
+  for(let i=0; i<accordionButtonContainer.length; i++) {
+    if(!accordionButtonContainer[i].contains(e.target)) {
+      accordion[i].style.height = '0';
+    }
+  }
+})
+
+
+let langs = document.querySelectorAll(".language__content div");
+
+for(let i=0; i<langs.length; i++) {
+  langs[i].addEventListener("click", function() {
+    for(let i=0; i<3; i++) {
+      langs[i].classList.remove("text-color-blue")
+    }
+    langs[i].classList.add("text-color-blue");
+  })
+}
